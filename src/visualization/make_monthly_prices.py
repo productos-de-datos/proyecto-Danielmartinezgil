a9bbc3fd3c1527a63c12c5f16531711a75e22540
Danielmartinezgil <damartinezgi@unal.edu.co>
2022-07-03 17:13:22 -0500

    Update make_monthly_prices.py

--- src/visualization/make_monthly_prices.py
+++ src/visualization/make_monthly_prices.py
@@ -1,25 +1,35 @@
 def make_monthly_prices_plot():
-    """Crea un grafico de lines que representa los precios promedios mensuales. 
+    """Crea un grafico de lines que representa los precios promedios diarios.
+    Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
+    lines que representa los precios promedios diarios.
+    El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
     """
-    #raise NotImplementedError("Implementar esta función")
     import pandas as pd
     import matplotlib.pyplot as plt
-    path_file = r'data_lake/business/precios-mensuales.csv'
-    datos = pd.read_csv(path_file, index_col=None, sep=',', header=0)
-    datos["fecha"] = pd.to_datetime(datos["fecha"])
-    x = datos.fecha
-    y = datos.precio
+    from datetime import datetime
 
-    plt.figure(figsize=(15, 6))
-    plt.plot(x, y, 'b', label='Promedio Mensual')
-    plt.title('Promedio Mensual')
-    plt.xlabel('Fecha')
-    plt.ylabel('Precio')
-    plt.legend()
-    plt.xticks(rotation="vertical")
-    plt.savefig("data_lake/business/reports/figures/monthly_prices.png")
+    def obtain_axix_plotting():
+        monthly_prices = pd.read_csv('data_lake/business/precios-mensuales.csv', header=0)
+        monthly_prices['Fecha'] = pd.to_datetime(monthly_prices['Fecha'], format='%Y-%m-%d')
+        X = monthly_prices['Fecha']
+        y = monthly_prices['Precio']
+        return X,y
+        
+    def plotting(X,y): 
+        plt.figure(figsize=(15, 6))
+        plt.plot(X, y, 'b', label='Prom. precio')
+        plt.title('Promedio de Precio Mensual de Energía')
+        plt.xlabel('Fecha')
+        plt.ylabel('Precio')
+        plt.legend()
+        plt.xticks(rotation="vertical")
+        plt.savefig("data_lake/business/reports/figures/monthly_prices.png")
+
+    X, y = obtain_axix_plotting()
+    plotting(X, y)
 
 if __name__ == "__main__":
     import doctest
-    make_monthly_prices_plot()
+
     doctest.testmod()
+    make_monthly_prices_plot()

9aa6c1a92dc8e52ebda9feda47b57788331e733b
Danielmartinezgil <damartinezgi@unal.edu.co>
2022-07-03 15:40:22 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -14,16 +14,20 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    import wget, os
+    import requests
 
-    os.chdir('data_lake/landing')
-    for num in range(1995, 2021):
-        
-        wdir = 'https://github.com/jdvelasq/datalabs/tree/master/datasets/precio_bolsa_nacional/xls/xxxx-{}.xls'.format(num)
-        wget.download(wdir)
+    for num in range(1995, 2022):
+        if num in range(2016, 2018):
+            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(num)
+            file = requests.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xls'.format(num), 'wb').write(file.content)
+        else:
+            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(num)
+            file = requests.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xlsx'.format(num), 'wb').write(file.content)
+    return
+    raise NotImplementedError("Implementar esta función")
 
-
-    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
